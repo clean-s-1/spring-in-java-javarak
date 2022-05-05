08ff7696bd22199c0f152e3f0fd57d79a629116e
javarak <99062807+javarak@users.noreply.github.com>
2022-05-05 15:26:48 +0530

    Update Statistics.java
    Added Generic type

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
+    public static Stats getStatistics(List<Integer> numbers) {
         //implement the computation of statistics here
     }
 }
