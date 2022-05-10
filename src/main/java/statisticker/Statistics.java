e6879af510891e80ac39197d26be606aafe090c6
javarak <99062807+javarak@users.noreply.github.com>
2022-05-10 23:35:12 +0530

    Update Statistics.java
    initialized member variable

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class Statistics {
 
 	static class Stats {
-		public float average;
-		public int min;
-		public int max;
+            public float average = 4.525f;
+            public float min = 1.5f;
+            public float max = 8.9f;
 	}
 
 	public static Stats getStatistics(List<Float> numbers) {
