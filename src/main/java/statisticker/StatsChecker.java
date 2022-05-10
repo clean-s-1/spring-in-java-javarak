74268d0052909ca35fafe6fc24b59c48e7d08951
javarak <99062807+javarak@users.noreply.github.com>
2022-05-10 23:36:20 +0530

    Create StatsChecker.java
    Added StatsChecker class

---
+++ src/main/java/statisticker/StatsChecker.java
@@ -0,0 +1,16 @@
+class StatsChecker {
+
+    private Float maxThreshold;
+    private IAlerter iAlerter[];
+
+    public StatsChecker(Float maxThreshold, IAlerter[] iAlerter) {
+        super();
+        this.maxThreshold = maxThreshold;
+        this.iAlerter = iAlerter;
+    }
+
+    public void checkAndAlert(Float[] numbers) {
+
+    }
+
+}
